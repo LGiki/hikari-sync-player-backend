afc385aa4bc2142edb2f86f81631f28e568c201b
LGiki <qq547221432@gmail.com>
2022-04-18 20:09:03 +0800

    add video

--- entity/media.go
+++ entity/media.go
@@ -1,9 +1,13 @@
 package entity
 
-type Episode struct {
+type PodcastEpisode struct {
 	Title        string `json:"title"`
 	PodcastName  string `json:"podcastName"`
 	CoverUrl     string `json:"coverUrl"`
 	EnclosureUrl string `json:"enclosureUrl"`
 	ThemeColor   string `json:"themeColor"`
+}
+
+type Video struct {
+	Url string `json:"title"`
 }
