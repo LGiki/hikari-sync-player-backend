16fac1963731431d1406b21f8694ee44412e3cd5
LGiki <qq547221432@gmail.com>
2022-04-18 19:58:39 +0800

    add error codes

--- pkg/e/code.go
+++ pkg/e/code.go
@@ -5,7 +5,14 @@ const (
 	ERROR         = 500
 	InvalidParams = 400
 
-	FailToParseXiaoYuZhouEpisode = 10000
+	FailToParseXiaoYuZhouEpisode    = 10000
+	FailToParseCowTransferShareLink = 10001
 
 	UnsupportedUrl = 20000
+
+	FailToSaveToRedis   = 30000
+	FailToReadFromRedis = 30001
+	KeyNotExists        = 30002
+
+	FailToUnmarshal = 40000
 )
--- pkg/e/msg.go
+++ pkg/e/msg.go
@@ -5,8 +5,13 @@ var MsgFlags = map[int]string{
 	ERROR:         "fail",
 	InvalidParams: "invalid params",
 
-	FailToParseXiaoYuZhouEpisode: "fail to parse xiaoyuzhou episode url",
-	UnsupportedUrl:               "unsupported url",
+	FailToParseXiaoYuZhouEpisode:    "fail to parse xiaoyuzhou episode url",
+	FailToParseCowTransferShareLink: "fail to parse cow transfer share link",
+	UnsupportedUrl:                  "unsupported url",
+	FailToSaveToRedis:               "fail to save to redis",
+	FailToReadFromRedis:             "fail to read from redis",
+	KeyNotExists:                    "key not exists",
+	FailToUnmarshal:                 "fail to unmarshal",
 }
 
 func GetMsg(code int) string {
